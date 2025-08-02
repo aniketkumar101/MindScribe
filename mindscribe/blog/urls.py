993b53e68ba4acd53322728cee43c74c69db17e1
Aniket Kumar <aniket787068@gmail.com>
2025-08-02 14:06:06 +0530

    change url of edit and delete

--- mindscribe/blog/urls.py
+++ mindscribe/blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
     path('create/', views.blog_create, name='blog_create'),
-    path('<int:blog_id>/edit/', views.blog_edit, name='blog_edit'),
-    path('<int:blog_id>/delete/', views.blog_delete, name='blog_delete'),
+    path('edit/<int:blog_id>/', views.blog_edit, name='blog_edit'),
+    path('delete/<int:blog_id>/', views.blog_delete, name='blog_delete'),
     
     path('register/', views.register, name='register'),
 ]
